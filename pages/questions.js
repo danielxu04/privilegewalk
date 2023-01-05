30696fca12e97325ac0b13281ba6886ce01d2c99
Daniel Xu <danielxu004@gmail.com>
2023-01-05 18:38:33 -0500

    Create questions.js
    array of questions for privilege walk questionnaire

---
+++ pages/questions.js
@@ -0,0 +1,229 @@
+const questions = [
+    {
+        id: 1,
+        questionNumber: '1',
+        question: "The leader of my country is also a person of my racial group.",
+        type: "RACE",
+        answer: true,
+        z: 'z-30'
+    },
+    {
+        id: 2,
+        questionNumber: '2',
+        question: "When I go shopping, I can easily find clothes that fit my size and shape.",
+        type: "SIZE",
+        answer: false,
+        z: 'z-29'
+    },
+    {
+        id: 3,
+        questionNumber: '3',
+        question: "In public, I can kiss and hold hands with the person I am dating without fear of name calling or violence.",
+        type: "SEXUALITY",
+        answer: false,
+        z: 'z-28'
+    },
+    {
+        id: 4,
+        questionNumber: '4',
+        question: "When I go shopping. I can be fairly certain that sales or security people will not follow me.",
+        type: "RACE/APPEARANCE",
+        answer: false,
+        z: 'z-27'
+    },
+    {
+        id: 5,
+        questionNumber: '5',
+        question: "Most of the religious and cultural holidays celebrated by my family are recognized with days off from work or school.",
+        type: "RELIGION/CULTURE",
+        answer: false,
+        z: 'z-26'
+    },
+    {
+        id: 6,
+        questionNumber: '6',
+        question: "When someone is trying to describe me, they do not mention my race.",
+        type: "RACE",
+        answer: false,
+        z: 'z-25'
+    },
+    {
+        id: 7,
+        questionNumber: '7',
+        question: "When I am angry or emotional, people do not dismiss my opinions as symptoms of “that time of the month.",
+        type: "GENDER",
+        answer: false,
+        z: 'z-24'
+    },
+    {
+        id: 8,
+        questionNumber: '8',
+        question: "When expressing my opinion, I am not automatically assumed to be a spokesperson of my race.",
+        type: "RACE",
+        answer: false,
+        z: 'z-23'
+    },
+    {
+        id: 9,
+        questionNumber: '9',
+        question: "I can easily buy greeting cards that represent my relationship with my significant other.",
+        type: "SEXUALITY",
+        answer: false,
+        z: 'z-22'
+    },
+    {
+        id: 10,
+        questionNumber: '10',
+        question: "I can easily find hair products and people who know how to style my hair.",
+        type: "RACE",
+        answer: false,
+        z: 'z-21'
+    },
+    {
+        id: 11,
+        questionNumber: '11',
+        question: "In my family, it is seen as normal to obtain a college degree.",
+        type: "CLASS",
+        answer: false,
+        z: 'z-20'
+    },
+    {
+        id: 12,
+        questionNumber: '12',
+        question: "If I am going out to dinner with friends, I do not worry if the building will be accessible to me.",
+        type: "ABILITY",
+        answer: false,
+        z: 'z-19'
+    },
+    {
+        id: 13,
+        questionNumber: '13',
+        question: "I can be certain that when I attend a work event there will be people of my race there.",
+        type: "RACE",
+        answer: false,
+        z: 'z-18'
+    },
+    {
+        id: 14,
+        questionNumber: '14',
+        question: "People do not make assumptions about my work ethic or intelligence based upon the size of my body.",
+        type: "SIZE",
+        answer: false,
+        z: 'z-17'
+    },
+    {
+        id: 15,
+        questionNumber: '15',
+        question: "When I strongly state my opinion, people see it as assertive rather than aggressive.",
+        type: "RACE/GENDER",
+        answer: false,
+        z: 'z-16'
+    },
+    {
+        id: 16,
+        questionNumber: '16',
+        question: "When I am with others of my race, people do not think that we are segregating ourselves.",
+        type: "RACE",
+        answer: false,
+        z: 'z-15'
+    },
+    {
+        id: 17,
+        questionNumber: '17',
+        question: "I can feel comfortable speaking about my culture without feeling that I'll be judged.",
+        type: "RACE/ETHNICITY",
+        answer: false,
+        z: 'z-14'
+    },
+    {
+        id: 18,
+        questionNumber: '18',
+        question: "I can usually afford (without much hardship) to do the things my friends want to do for entertainment.",
+        type: "CLASS",
+        answer: false,
+        z: 'z-13'
+    },
+    {
+        id: 19,
+        questionNumber: '19',
+        question: "When filling out forms for school or work, I easily identify with the box that I have to check.",
+        type: "GENDER/RACE",
+        answer: false,
+        z: 'z-12'
+    },
+    {
+        id: 20,
+        questionNumber: '20',
+        question: "I can choose the style of dress that I feel comfortable in and most reflects my identity, and I know that I will not be stared at in public.",
+        type: "GENDER/APPEARANCE",
+        answer: false,
+        z: 'z-11'
+    },
+    {
+        id: 21,
+        questionNumber: '21',
+        question: "If pulled over by a police officer, I can be sure that I have not been singled out because of my race.",
+        type: "RACE",
+        answer: false,
+        z: 'z-10'
+    },
+    {
+        id: 22,
+        questionNumber: '22',
+        question: "My professionalism is never questioned because of my age.",
+        type: "AGE",
+        answer: false,
+        z: 'z-9'
+    },   
+    {
+        id: 23,
+        questionNumber: '23',
+        question: "I do not worry about walking alone at night.",
+        type: "GENDER",
+        answer: false,
+        z: 'z-8'
+        },   
+    {
+        id: 24,
+        questionNumber: '24',
+        question: "People do not make assumptions about my intelligence based upon my style of speech.",
+        type: "RACE",
+        answer: false,
+        z: 'z-7'
+    },   
+    {
+        id: 25,
+        questionNumber: '25',
+        question: "When attending class or other events, I do not have to worry about having an interpreter present to understand or to participate.",
+        type: "ABILITY/LANGUAGE",
+        answer: false,
+        z: 'z-6'
+    },   
+    {
+        id: 26,
+        questionNumber: '26',
+        question: "I can book an airline flight, go to a movie, ride in a car and not worry about whether there will be a seat that can accommodate me.",
+        type: "SIZE/ABILITY",
+        answer: false,
+        z: 'z-5'
+    },   
+    {
+        id: 27,
+        questionNumber: '27',
+        question: "People assume I was admitted to school or hired based upon my credentials, rather than my race or gender.",
+        type: "RACE/GENDER",
+        answer: false,
+        z: 'z-4'
+    },   
+    {
+        id: 28,
+        questionNumber: '28',
+        question: "As a child, I could use the 'flesh-colored' crayons to colour my family and have it match our skin colour.",
+        type: "RACE",
+        answer: false,
+        z: 'z-3'
+    }
+]
+
+
+export default questions;

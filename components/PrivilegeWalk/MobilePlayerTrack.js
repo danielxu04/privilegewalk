954986d9420ef2253909aaacefb0d4c7c8f4b9a0
Daniel Xu <danielxu004@gmail.com>
2023-02-22 15:13:43 -0500

    fixed mobile player positioning

--- components/PrivilegeWalk/MobilePlayerTrack.js
+++ components/PrivilegeWalk/MobilePlayerTrack.js
@@ -6,7 +6,7 @@ function MobileTrack(props) {
     return (
         <div style={{ position: "relative" }} className="flex mx-auto h-[100%] w-[11.4vw] border-2 mobile-player-track b-2 border-zinc-700 
         rounded-lg">
-            <div style={{ position: "absolute", bottom: 0 }} className={`flex pl-1 ${props.mobileUserPadding}`}>
+            <div style={{ bottom: 0, right: 0 }} className={`flex absolute ${props.mobileUserPadding}`}>
                 <MobileWalkingPerson walkBool={props.walkBool} setWalkBool={props.setWalkBool} image={WalkingMan} />
             </div>
         </div>
--- components/PrivilegeWalk/MobileTrack.js
+++ components/PrivilegeWalk/MobileTrack.js
@@ -6,7 +6,7 @@ import MobileWalkingPerson from './MobileWalkingPerson';
 function MobileTrack(props) {
     return (
         <div style={{ position: "relative" }} className="mx-auto h-[100%] w-[11.4vw] border-2 bg-black border-zinc-600 rounded-lg">
-            <div style={{ position: "absolute", bottom: 0 }} className={`pl-1 ${props.mobilePlayerPadding}`}>
+            <div style={{bottom: 0, right: 0}} className={`pl-1 absolute ${props.mobilePlayerPadding}`}>
                 <MobileWalkingPerson walkBool={props.walkBool} setWalkBool={props.setWalkBool} image={WalkingMan} />
             </div>
         </div>

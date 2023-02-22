8deb4db3b04fe1ef1dc481a6a74b5ffdbfcff6a8
Daniel Xu <danielxu004@gmail.com>
2023-02-22 00:31:02 -0500

    added gridlines/checkpoints

--- components/PrivilegeWalk/PlayerTrack.js
+++ components/PrivilegeWalk/PlayerTrack.js
@@ -2,8 +2,37 @@ import WalkingPerson from './WalkingPerson';
 
 function PlayerTrack(props) {
     return (
-        <div className="flex text-white text-2xl pt-9 px-2 z-10 player-track darkcream-border border-2 w-[100%] cream-boxshadow">
-            <div className={`flex ${props.playerPadding}`}>
+        <div className="relative h-[5vw] px-2 darkcream-border w-[100%] cream-boxshadow">
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[3vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[6vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[9vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[12vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[15vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[18vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[21vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[24vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[27vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[30vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[33vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[36vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[39vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[42vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[45vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[48vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[51vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[54vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[57vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[60vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[63vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[66vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[69vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[72vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[75vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[78vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[81vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[84vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[87vw]"></span>
+            <div className={`absolute top-[48%] pl-[0.5vw] ${props.playerPadding}`}>
                 <WalkingPerson walkBool={props.walkBool} setWalkBool={props.setWalkBool} image={props.playerImage}/>
             </div>
         </div>
--- components/PrivilegeWalk/Track.js
+++ components/PrivilegeWalk/Track.js
@@ -2,8 +2,37 @@ import WalkingPerson from './WalkingPerson';
 
 function Track(props) {
     return (
-        <div className="flex pt-8 px-2 darkcream-border border-b-2 w-[100%]">
-            <div className={`flex ${props.playerPadding}`}>
+        <div className="relative h-[5vw] px-2 darkcream-border border-b-2 w-[100%]">
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[3vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[6vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[9vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[12vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[15vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[18vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[21vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[24vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[27vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[30vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[33vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[36vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[39vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[42vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[45vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[48vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[51vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[54vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[57vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[60vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[63vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[66vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[69vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[72vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[75vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[78vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[81vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[84vw]"></span>
+            <span className="absolute bg-white border-white w-[0.01vw] h-[100%] left-[87vw]"></span>
+            <div className={`absolute top-[49%] pl-[0.5vw] ${props.playerPadding}`}>
                 <WalkingPerson walkBool={props.walkBool} setWalkBool={props.setWalkBool} image={props.playerImage}/>
             </div>
         </div>

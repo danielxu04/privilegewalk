62d7005a1059500426a6e55b1e1b0e1a47935111
Daniel Xu <danielxu004@gmail.com>
2023-02-23 23:54:57 -0500

    changed padding on start page button

--- components/StartPage/StartButton.js
+++ components/StartPage/StartButton.js
@@ -8,7 +8,7 @@ function StartButton(){
     return(
             <div 
                 className="flex hover:cursor-pointer hover:scale-105 font-interMedium text-lg transition-050s justify-center rounded-lg 
-                cream-background start-btn mt-[5vw] px-6 py-2 xl:w-[8vw] xl:px-3 xl:mt-0" 
+                cream-background start-btn mt-[1vw] px-6 py-2 xl:w-[8vw] xl:px-3 xl:mt-0" 
                 onClick={() => dispatch({ type: 'start' })}>
                     Start
             </div>
